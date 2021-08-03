8a81b0611d05b1294f1c6161a1c503da1ca27075
M-a-n-a <79129055+M-a-n-a@users.noreply.github.com>
2021-08-03 16:05:13 +1000

    Added back-to-top button...
    ...But it doesn't work show on the case study page

--- js/javascript.js
+++ js/javascript.js
@@ -1,4 +1,31 @@
+//Animated menu button
 document.querySelector('.menu-button').addEventListener('click', function () {
 
 document.querySelector('.animated-menu-icon').classList.toggle('open');
 });
+
+//Get the back-to-top button
+let mybutton = document.getElementById("btn-back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the back-to-top button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 20 ||
+    document.documentElement.scrollTop > 20
+  ) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+// When the user clicks on the back-to-top button, scroll to the top of the document
+mybutton.addEventListener("click", backToTop);
+
+function backToTop() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
